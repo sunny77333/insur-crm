eccaa9b63862f9cecbca74883f57362f590bedb3
Sunny <128038119+sunny77333@users.noreply.github.com>
2026-02-25 17:41:46 +1000

    Initialize Spring Boot project

---
+++ src/main/java/com/insurecrm/InsureCrmApplication.java
@@ -0,0 +1,13 @@
+package com.insurecrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsureCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsureCrmApplication.class, args);
+	}
+
+}
